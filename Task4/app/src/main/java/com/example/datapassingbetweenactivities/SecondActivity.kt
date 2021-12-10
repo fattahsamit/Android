fe736d36f6b8c4774f6c03212a32eabbadc22070
fattahsamit <fattahsamit@gmail.com>
2021-12-10 17:01:57 +0600

    TextView to display the data stored inside finalString variable which holds all the EXTRA data from intent

--- Task4/app/src/main/java/com/example/datapassingbetweenactivities/SecondActivity.kt
+++ Task4/app/src/main/java/com/example/datapassingbetweenactivities/SecondActivity.kt
@@ -2,10 +2,19 @@ package com.example.datapassingbetweenactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val name = intent.getStringExtra("EXTRA_NAME")
+        val age = intent.getStringExtra("EXTRA_AGE")
+        val country = intent.getStringExtra("EXTRA_COUNTRY")
+
+        val finalString = "$name is $age years old who lives in $country"
+
+        textView.text =finalString
     }
 }
