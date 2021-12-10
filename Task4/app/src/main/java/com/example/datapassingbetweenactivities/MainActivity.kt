0cf1bb3e373af4da73bf0b9dd95364e4c1b297de
fattahsamit <fattahsamit@gmail.com>
2021-12-10 16:39:17 +0600

    Using Intent function to another activity by clicking button

--- Task4/app/src/main/java/com/example/datapassingbetweenactivities/MainActivity.kt
+++ Task4/app/src/main/java/com/example/datapassingbetweenactivities/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.datapassingbetweenactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSubmit.setOnClickListener {
+            Intent(this, SecondActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
     }
 }
