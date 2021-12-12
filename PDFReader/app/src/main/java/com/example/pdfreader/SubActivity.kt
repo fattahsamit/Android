12b4b10113df3f6ad64ba2f6901b6b7b2b9eaf19
fattahsamit <fattahsamit@gmail.com>
2021-12-12 16:41:27 +0600

    Load file from Asset

--- PDFReader/app/src/main/java/com/example/pdfreader/SubActivity.kt
+++ PDFReader/app/src/main/java/com/example/pdfreader/SubActivity.kt
@@ -2,10 +2,13 @@ package com.example.pdfreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+
+        pdfview.fromAsset("Lecture1.pdf").load()
     }
 }
